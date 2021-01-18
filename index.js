c7ec3949306cde8202866f96e028c061fb8d060f
Sriraj Bura <sriraj.bura16@gmail.com>
2021-01-18 12:53:37 -0500

    finished adding grid

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 //Selectors
-const container = document.createElement('div');
-container.id = "container"
+const container = document.querySelector('#container');
 const main = document.querySelector('main');
+const cell = document.querySelector('.cell');
+
 
 //Functions
 function createGrid(num){
-    container.style.setProperty("--grid-rows", num)
-    container.style.setProperty("--grid-cols", num)
+    container.style.setProperty("--grid-nums", num)
     for(let i = 1; i <= (num * num); i++){
-        const divs = document.createElement('div');
-        divs.textContent = i
-        container.appendChild(divs).className = "cell"
+        const div = document.createElement('div');
+        div.onmouseover = function(){
+            div.style.backgroundColor = 'red'
+        }
+        container.appendChild(div).className = 'cell'
     };
     main.appendChild(container)
 };
-createGrid(16)
-
-
+createGrid(100)
